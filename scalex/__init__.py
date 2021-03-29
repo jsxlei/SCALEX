2881929521a031367e9930639f431394abe62f7f
jsxlei <jsxlei@gmail.com>
2021-03-29 20:22:18 +0800

    rename SCALE v2 to SCALEX

--- scalex/__init__.py
+++ scalex/__init__.py
@@ -1,7 +1,7 @@
 # from pkg_resources import get_distribution
 
 # __version__ = get_distribution('scalex').version
-__version__ = '2.0.0.dev0'
+__version__ = '0.2.0'
 __author__ = 'Lei Xiong'
 __email__ = 'jsxlei@gmail.com'
 
